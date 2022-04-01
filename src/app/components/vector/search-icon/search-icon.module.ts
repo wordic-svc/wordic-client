4eb26a4ff0927c9cda6ca435e593a2f3256bdff5
loeaf <ckdic8524@naver.com>
2022-04-01 22:49:15 +0900

    서치 아이콘 추가

---
+++ src/app/components/vector/search-icon/search-icon.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchIconComponent } from './search-icon.component';
+
+
+
+@NgModule({
+  declarations: [
+    SearchIconComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SearchIconComponent
+  ]
+})
+export class SearchIconModule { }
