91e3fa5018842daec9395e3d30580c7b35aef621
loeaf <ckdic8524@naver.com>
2022-04-13 15:11:51 +0900

    로띠 트리거 수정

--- src/app/components/common/lotti/lotti.component.ts
+++ src/app/components/common/lotti/lotti.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { AnimationOptions } from 'ngx-lottie';
 import { AnimationItem } from 'ngx-lottie/lib/symbols';
 import { LottiService } from './lotti.service';
@@ -8,10 +8,18 @@ import { LottiService } from './lotti.service';
   templateUrl: './lotti.component.html',
   styleUrls: ['./lotti.component.css']
 })
-export class LottiComponent implements OnInit{
+export class LottiComponent implements OnInit, OnDestroy{
   show = false;
-  constructor(private lottiSvc: LottiService) {
+  constructor(private lottiSvc: LottiService, private cdr: ChangeDetectorRef) {
+    this.lottiSvc.toggle.subscribe((state) => {
+      this.show = state;
+      this.cdr.detectChanges(); // Manually trigger change detection
+    });
   }
+
+  ngOnDestroy(): void {
+    this.lottiSvc.toggle.unsubscribe();
+    }
 
   options: AnimationOptions = {
     path: './assets/animation.json',
@@ -22,8 +30,5 @@ export class LottiComponent implements OnInit{
   }
 
   ngOnInit (): void {
-    this.lottiSvc.toggle.subscribe(() => {
-      this.show = !this.show;
-    });
   }
 }
