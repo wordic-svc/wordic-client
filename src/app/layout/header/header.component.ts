1dae342ae667c122e878133cd9c8b945b4013bdb
loeaf <ckdic8524@naver.com>
2022-05-01 00:30:03 +0900

    헤더 수정

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit{
   isLogin: boolean = false;
-  constructor () {
+  constructor (private location: Location) {
   }
   ngOnInit (): void {
   }
 
+  onMove (engAbbreviate: string) {
+    location.href = `/${engAbbreviate}`;
+  }
 }
