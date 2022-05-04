a7fd98d8876aa35aa05e4915ae701cc73c4fb89d
loeaf <ckdic8524@naver.com>
2022-05-05 00:43:29 +0900

    경로이동 수정

--- src/app/components/mixin/hamburger/hamburger.component.ts
+++ src/app/components/mixin/hamburger/hamburger.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 export class HamburgerComponent {
   isOpen: boolean = false;
 
+  onMove (engAbbreviate: string) {
+    location.href = `/${engAbbreviate}`;
+  }
 }
