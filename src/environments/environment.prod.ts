4ad321335a1c500580376c6b578583376f5f0476
loeaf <ckdic8524@naver.com>
2023-07-22 01:12:26 +0900

    env 수정

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '/wordic-api', // API 엔드포인트 또는 다른 환경 변수 설정
+  apiUrl: 'https://wordic.loeaf.com/wordic-api', // API 엔드포인트 또는 다른 환경 변수 설정
   i18nPath: '/assets/i18n/', // 번역 파일 경로
-  basePath: '', // 기본 경로
+  basePath: 'https://wordic.loeaf.com', // 기본 경로
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://127.0.0.1:8000/wordic-api', // API 엔드포인트 또는 다른 환경 변수 설정
   i18nPath: '/assets/i18n/', // 번역 파일 경로
-  basePath: '', // 기본 경로
+  basePath: 'https://wordic.loeaf.com', // 기본 경로
 };
