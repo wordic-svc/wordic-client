9f060bd389666eeb7835448384dc052971db0311
loeaf <ckdic8524@naver.com>
2023-08-25 22:42:22 +0900

    tailwind 설정

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'oh-my-baby';
+  constructor(private translateService: TranslateService) {
+    // 초기 언어 설정
+    translateService.setDefaultLang('ko'); // 한국어로 설정
+    // 번역된 문자열 가져오기
+    this.translateService.use('ko'); // 언어 변경
+    const translatedText = this.translateService.instant('Helloworld');
+    console.log(translatedText); // 번역된 문자열 출력
+  }
+
+  // 다른 로캘로 변경하는 함수
+  switchLocale(locale: string) {
+    this.translateService.use(locale);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';;
+
+// TranslateLoader 초기화 함수
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
