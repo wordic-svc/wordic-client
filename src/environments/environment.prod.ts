c7e704ead77299c06419768da2703610e033c68f
loeaf <ckdic8524@naver.com>
2023-11-04 21:14:35 +0900

    https 지원

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://wordic.loeaf.com/wordic-api', // API 엔드포인트 또는 다른 환경 변수 설정
+  apiUrl: '/wordic-api', // API 엔드포인트 또는 다른 환경 변수 설정
   i18nPath: '/assets/i18n/', // 번역 파일 경로
   basePath: '', // 기본 경로
 };
