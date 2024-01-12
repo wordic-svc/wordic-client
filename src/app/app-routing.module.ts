ddda55e6e66619e239d07916960f0adf9497ab6e
loeaf <ckdic8524@naver.com>
2024-01-12 23:32:43 +0900

    라우팅 추가

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { IntroComponent } from './pages/intro/intro.component';
 import { EngAbbreviateComponent } from './pages/eng-abbreviate/eng-abbreviate.component';
+import { CaseConvertComponent } from './pages/case-convert/case-convert.component';
 
 const routes: Routes = [
   { path: '',  redirectTo: '/variable-name', pathMatch: 'full' },
   { path: 'variable-name', component: HomeComponent},
   { path: 'eng-abbreviate', component: EngAbbreviateComponent},
+  { path: 'case-convert', component: CaseConvertComponent},
   { path: 'intro', component: IntroComponent},
 ];
 
