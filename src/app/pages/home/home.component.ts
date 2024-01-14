d4701845df1b373baced4f6266f9f59d1954d645
loeaf <ckdic8524@naver.com>
2024-01-14 11:26:28 +0900

    업데이트

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -18,9 +18,9 @@ export class HomeComponent implements OnInit{
       author: 'wordic',
       keywords: `한글 변수명, 영어 변수명, 컬럼명, 영문 약어, 영어약자, 변수 영어로, 변수 선언, 변수명 짓기, 컬럼명 짓기, 영문약어, 영어약자, 변수 영어로, 변수 선언, 2023년 공공데이터 표준, 2022년 공공데이터 표준, 2021년 공공데이터 표준`,
       lang: 'ko',
-      title: `변수명 생성기 Wordic | 변수명 짓기 | 컬럼명 짓기 | 변수명 약어 | 컬럼명 약어 | 공공데이터 표준`,
+      title: `공공데이터 공통표준용어 변수명 생성기 Wordic | 변수명 짓기 | 컬럼명 짓기 | 변수명 약어 | 컬럼명 약어 | 공공데이터 표준`,
       description: `한글명을 영문 축약어로 쉽게 만들 수 있습니다. 변수명, 컬럼명, 필드명, 영어 약어, 영문약어 등.`,
-      site_name: '변수명 생성기 Wordic',
+      site_name: '공공데이터 공통표준용어 변수명 생성기 Wordic',
       site_url: environment.basePath + '/variable-name',
       src: environment.basePath + '/variable-name'
     }
